d2f76ea407e6c38a568d21015a4177aeed2bacb0
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-04-01 10:22:42 -0700

    Add project files.

---
+++ DrakiaXYZTaskListFixesPlugin.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace DrakiaXYZ_TaskListFixes
+{
+    [BepInPlugin("xyz.drakia.tasklistfixes", "DrakiaXYZ-TaskListFixes", "0.0.1")]
+    public class DrakiaXYZTaskListFixesPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+
+        }
+    }
+}
