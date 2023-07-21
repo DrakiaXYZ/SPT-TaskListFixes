7222a29ff683c1a21f12fdc44b457b406e52b93e
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-07-20 19:46:13 -0700

    - Rebuild for new EFT version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: TarkovVersion(24605)]
+[assembly: TarkovVersion(24964)]
